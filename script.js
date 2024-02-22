d68d44ebc02bff0df5370a2594c1050e09d5a828
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-02-22 17:57:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.eventListner('DOMContentLoaded',()=>{
+document.addEventListner('DOMContentLoaded',()=>{
 	document.body.innertext("DOM load success");
 })
